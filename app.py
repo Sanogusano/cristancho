27ccd9bf579f1d58c66ec764c77c25eb5196475e
Andres Felipe Restrepo Henao <anfereh@gmail.com>
2026-05-07 13:40:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from PIL import Image as _PIL_Image
+_APP_ICON = _PIL_Image.open("assets/mascot.png")
 import streamlit as st
 import pandas as pd
 from utils import db
@@ -5,7 +7,7 @@ from utils import style
 
 st.set_page_config(
     page_title="Cristancho",
-    page_icon="⛏️",
+    page_icon=_APP_ICON,
     layout="wide",
 )
 style.apply()
